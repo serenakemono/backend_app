d6d8d9075577820b909ed381cf47c1bf86bc6b39
Serena <siriuswusss@gmail.com>
2022-07-04 14:58:06 +0800

    Refactor controllers

--- app/customer_order/controllers.py
+++ app/customer_order/controllers.py
@@ -20,6 +20,18 @@ def get_customers():
     else:
         customers = Customer.query.order_by(desc(Customer.dob)).limit(number).all()
 
+    all_customers = retrieve_customers_info(customers)
+
+    return jsonify(
+        {
+            "success": True,
+            "customers": all_customers,
+            "total_customers": len(customers),
+        }
+    )
+
+
+def retrieve_customers_info(customers):
     all_customers = []
     for customer in customers:
         orders = []
@@ -35,14 +47,7 @@ def get_customers():
             }
         }
         all_customers.append(results)
-
-    return jsonify(
-        {
-            "success": True,
-            "customers": all_customers,
-            "total_customers": len(customers),
-        }
-    )
+    return all_customers
 
 
 @app.route('/order', methods=['GET'])
@@ -53,16 +58,7 @@ def get_orders():
     else:
         orders = Order.query.filter_by(customer_id=customer_id).all()
 
-    all_orders = []
-    for order in orders:
-        results = {
-            "order_id": order.id,
-            "item_name": order.item_name,
-            "item_price": order.item_price,
-            "order_time": order.datetime,
-            "customer_id": order.customer_id,
-        }
-        all_orders.append(results)
+    all_orders = retrieve_orders_info(orders)
 
     return jsonify(
         {
@@ -73,25 +69,25 @@ def get_orders():
     )
 
 
+def retrieve_orders_info(orders):
+    all_orders = []
+    for order in orders:
+        results = {
+            "order_id": order.id,
+            "item_name": order.item_name,
+            "item_price": order.item_price,
+            "order_time": order.datetime,
+            "customer_id": order.customer_id,
+        }
+        all_orders.append(results)
+    return all_orders
+
+
 @app.route('/customer/create', methods=['POST'])
 def create_customer():
-    customer_data = request.json
-    try:
-        name = customer_data["name"]
-        dob = customer_data["dob"]
-    except KeyError:
-        abort(400, description="Customer name and/or dob missing")
-
-    if name == "":
-        abort(404, description="Name cannot be empty")
-
-    try:
-        date = datetime.datetime.strptime(dob, '%Y-%m-%d')
-        present = datetime.datetime.now()
-        if present.date() < date.date():
-            abort(404, description="Invalid date, should be before today")
-    except ValueError:
-        abort(404, description="Incorrect date format, should be YYYY-mm-dd")
+    name, dob = retrieve_customer_params(request.json)
+    validate_customer_name(name)
+    validate_customer_dob(dob)
 
     customer = Customer(name=name, dob=dob)
     db.session.add(customer)
@@ -102,4 +98,28 @@ def create_customer():
         "customer_name": customer.name,
         "customer_dob": customer.dob.strftime("%Y-%m-%d"),
     })
+
+
+def retrieve_customer_params(customer_data):
+    try:
+        name = customer_data["name"]
+        dob = customer_data["dob"]
+        return name, dob
+    except KeyError:
+        abort(404, description="Customer name and/or dob missing")
+
+
+def validate_customer_name(name):
+    if name == "":
+        abort(400, description="Name cannot be empty")
+
+
+def validate_customer_dob(dob):
+    try:
+        date = datetime.datetime.strptime(dob, '%Y-%m-%d')
+        present = datetime.datetime.now()
+        if present.date() < date.date():
+            abort(400, description="Invalid date, should be before today")
+    except ValueError:
+        abort(400, description="Incorrect date format, should be YYYY-mm-dd")
 
